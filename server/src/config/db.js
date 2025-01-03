f22bbee603806789a57789abb83b3a437c32f619
craig91 <craigdejean1@gmail.com>
2025-01-03 18:41:55 -0500

    fix: issue with database connection and environment variable

---
+++ server/src/config/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config({ path: '../.env' });
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+
+
+pool.getConnection()
+    .then(() => console.log('Database connected successfully!'))
+    .catch((err) => console.error('Database connection failed:', err.message));
+
+module.exports = pool;
---
+++ server/src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const pool = require('./config/db');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+
+// app.get('/', (req, res)=> {
+//     res.send("API is running");
+// });
+
+app.get('/db-test', async (req, res) => {
+    try {
+        const[rows] = await pool.query('SELECT 1 + 1 AS solution');
+        res.join({ result: rows[0].result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Database connection failed");
+    }
+})
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
