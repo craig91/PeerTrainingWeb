37b2d506b68ad1b0192a240cab9a8a174692c270
craig91 <craigdejean1@gmail.com>
2025-01-03 19:35:35 -0500

    fix: fixed path to env variable issues

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const pool = require('./config/db');
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -15,7 +15,8 @@ app.use(express.json());
 app.get('/db-test', async (req, res) => {
     try {
         const[rows] = await pool.query('SELECT 1 + 1 AS solution');
-        res.join({ result: rows[0].result });
+        console.log('Query Result: ', rows);
+        res.json({ result: rows[0].result });
     } catch (error) {
         console.error(error);
         res.status(500).send("Database connection failed");
