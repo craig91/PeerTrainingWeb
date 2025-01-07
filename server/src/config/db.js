7f5488c8a8d1a880d86e61a6f30965634ba68306
craig91 <craigdejean1@gmail.com>
2025-01-07 11:39:02 -0500

    chore: removed console.log functions from db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql2/promise');
-// require('dotenv').config({ path: '../.env' });
-// require('dotenv').config();
+
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -8,11 +7,6 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
-
-// console.log('DB_HOST:', process.env.DB_HOST);
-// console.log('DB_USER:', process.env.DB_USER);
-// console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-// console.log('DB_NAME:', process.env.DB_NAME);
 
 
 pool.getConnection()
